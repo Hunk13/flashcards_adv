6b6d415b1331655a3edfc3f36a1aa25b60f4fdea
Aliaksandr Kalinka <alex.kalinoff@gmail.com>
2015-10-24 16:50:09 +0300

    first cotereview commit fix hound errors

--- app/controllers/home/oauths_controller.rb
+++ app/controllers/home/oauths_controller.rb
@@ -8,17 +8,17 @@ class Home::OauthsController < Home::BaseController
   def callback
     provider = auth_params[:provider]
     if @user = login_from(provider)
-      redirect_to trainer_path, notice: (I18n.t("log_in_is_successful_provider_notice"),
+      redirect_to trainer_path, notice: (t "log_in_is_successful_provider_notice",
                                          provider: provider.titleize)
     else
       begin
         @user = create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to trainer_path, notice: (I18n.t("log_in_is_successful_provider_notice"),
+        redirect_to trainer_path, notice: (t "log_in_is_successful_provider_notice",
                                            provider: provider.titleize)
       rescue
-        redirect_to user_sessions_path, alert: (I18n.t("log_out_failed_provider_alert"),
+        redirect_to user_sessions_path, alert: (t "log_out_failed_provider_alert",
                                                 provider: provider.titleize)
       end
     end
